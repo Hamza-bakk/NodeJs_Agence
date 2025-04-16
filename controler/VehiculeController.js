3aed1d78d442f4946430ac781b4bffbeadf54e21
Salah-din <salahdinbenaouda@gmail.com>
2025-04-16 16:29:49 +0200

    update salah vehicule et agence

--- controler/VehiculeController.js
+++ controler/VehiculeController.js
@@ -11,3 +11,16 @@ export const GetAllVehicule = async (req, res) => {
     throw new Error("Internal server error");
   }
 };
+export const UpdateVehicule = async (req, res) => {
+  try {
+    const vehicule = await Vehicule.update(req.body, {
+      where: { id: req.params.id },
+    });
+    vehicule
+      ? res.json("Vehicule updated successfully")
+      : res.status(404).json({ error: "Vehicule not found" });
+  } catch (error) {
+    console.error("Error updating Vehicule:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
